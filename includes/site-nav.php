a44108cecd72bd48bb5d8e2da3f2fd418f3e2196
Austin Cornelio <austin@yhmg.com>
2012-11-06 18:24:50 -0500

    initial commit

---
+++ includes/site-nav.php
@@ -0,0 +1,45 @@
+<nav id="menu" class="navigation">
+<ul id="nav">
+  <li><a href="index.html" class="active">The YH Difference</a>
+    <ul>
+      <li><a href="index_2.html">About Us</a></li>
+      <li><a href="index_3.html">Management Team</a></li>
+    </ul>
+  </li>
+  <li><a href="#1">Services</a>
+    <ul>
+      <li><a href="#">Media Buying &amp; Distrobution</a></li>
+      <li><a href="#">Optimizing &amp; Targeting</a></li>
+      <li><a href="#">Creative</a></li>
+      <li><a href="#">Reporting &amp; Transparency</a></li>
+      <li><a href="#">Tracking Platform</a></li>
+    </ul>
+  </li>
+  <li><a href="#2">Clients</a>
+    <ul>
+      <li><a href="shortcodes.html">Clients Overview</a></li>
+      <li><a href="icons.html">Client Case Studies</a></li>
+      <li><a href="pricing_tables.html">Client Dashboard Login</a></li>
+      <li><a href="under_construction.html">OmniTarget Login</a></li>
+    <ul>
+      <li><a href="#3-1">Sub Item Example 1</a></li>
+          <li><a href="#3-2">Sub Item Example 2</a></li>
+    </ul>
+      </li>
+    </ul>
+  </li>
+  <li><a href="portfolio_4.html#4">News</a>
+    <ul>
+      <li><a href="portfolio_2.html">Press Releases</a></li>
+      <li><a href="portfolio_3.html">The Blog</a></li>
+    </ul>
+  </li>
+  <li><a href="blog.html#5">Careers</a>
+    <ul>
+      <li><a href="blog.html">Job Openings</a></li>
+      <li><a href="blog_2.html">Internships</a></li>
+    </ul>
+  </li>
+  <li><a href="contact.html">Contact</a></li>
+</ul>
+</nav>
